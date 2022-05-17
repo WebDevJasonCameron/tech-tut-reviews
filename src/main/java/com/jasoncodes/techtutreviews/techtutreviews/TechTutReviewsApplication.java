7cd8d34613df7da886a5570a5432ad202bc51491
Jason Cameron <dev.jasoncameron@gmail.com>
2022-05-16 21:32:03 -0500

    start

---
+++ src/main/java/com/jasoncodes/techtutreviews/techtutreviews/TechTutReviewsApplication.java
@@ -0,0 +1,13 @@
+package com.jasoncodes.techtutreviews.techtutreviews;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechTutReviewsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TechTutReviewsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jasoncodes/techtutreviews/techtutreviews/TechTutReviewsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jasoncodes.techtutreviews.techtutreviews;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechTutReviewsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
